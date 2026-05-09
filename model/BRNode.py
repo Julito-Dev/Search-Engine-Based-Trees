09a04075464dc46dc4dfa0dbc6695f3436994779
Julito-Dev <julianpaez180@gmail.com>
2026-05-09 12:52:23 -0500

    FIx: Class RBNode renamed and constants were defined correctly

--- model/BRNode.py
+++ model/BRNode.py
@@ -1,11 +1,10 @@
 from Node import Node
+RED = "RED"
+BLACK = "BLACK"
 
-class BRNode(Node):
+class RBNode(Node):
     def __init__(self, key, data):
         super().__init__(key, data)
         
         self.color = RED #Un nodo recien creado es rojo.
         self.parent = None
-        
-RED = "RED"
-BLACK = "BLACK"
