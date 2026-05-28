25aaf2cb4d1e410b5e851fdde180927c8857c726
Julito-Dev <julianpaez180@gmail.com>
2026-05-27 20:04:03 -0500

    feat: INSERT INTO, DELETE FROM commands parsed. Parser Class

--- REPL/Parser.py
+++ REPL/Parser.py
@@ -34,3 +34,23 @@ class Parser:
         m = re.match(r"CREATE\s+TABLE\s+(\w+)\s+(AVL|RB)", cmd, re.IGNORECASE)
         if m:
             return {"action": "create_table", "table": m.group(1), "tree_type": m.group(2).upper()}
+    
+        # INSERT INTO <tabla> <key> <json>
+        
+        m = re.match(r"INSERT\s+INTO\s+(\w+)\s+(\d+)\s+(\{.*\}))", cmd, re.IGNORECASE)
+        
+        if m:
+            try:
+                data = json.loads(m.group(3))
+            
+            except json.JSONDecodeError:
+                raise ValueError("El JSON no es Valido.")
+            return {"action": "insert", "table": m.group(1), "key": int(m.group(2)), "data":data}
+        
+        
+        # DELETE FROM <from> <key>
+        
+        m = re.match(r"DELETE\s+FROM\s+(\w+)\s+(\d+)", cmd, re.IGNORECASE)
+        
+        if m:
+            return {"action": "delete", "table": m.group(1), "key": int(m.group(2))}
